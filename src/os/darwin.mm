b10b38eab9b696eb681b39ccb7ef526bf5430f2d
felix <felix.schuelke@gmx.net>
2023-09-07 02:37:02 +0200

    added open_file impl

--- src/os/darwin.mm
+++ src/os/darwin.mm
@@ -1,6 +1,8 @@
-#include "sklib/os/filedialog.hpp"
 #include <AppKit/AppKit.h>
-#include <sklib/os/file_dialog.hpp>
+#include <filesystem>
+
+#include <sklib/os/filedialog.hpp>
+
 #import <Cocoa/Cocoa.h>
 
 // MACOS implementation
@@ -9,11 +11,15 @@ namespace sklib::os
 {
     file_dialog_result open_file(const file_dialog_options &options)
     {
+        using mask = file_dialog_options::mask;
+
         NSOpenPanel *openDlg = [NSOpenPanel openPanel];
 
+        // add assertions for flags that are not supported but set regardless 
+
         [openDlg setCanChooseFiles:YES];
-        [openDlg setCanChooseDirectories:NO];
-        [openDlg setAllowsMultipleSelection:multiple];
+        [openDlg setCanChooseDirectories:NO]; // TODO add option for choosing directories
+        [openDlg setAllowsMultipleSelection:options.is_flag_set(mask::allow_multiple)];
         NSMutableArray *types = [NSMutableArray new];
         // for (size_t idx = 0; idx < filetypes.size(); ++idx)
         //    [types addObject: [NSString stringWithUTF8String: filetypes[idx].first.c_str()]];
@@ -21,11 +27,16 @@ namespace sklib::os
 
 
         [openDlg setAllowedFileTypes: types];
+        std::vector<std::filesystem::path> paths;
 
         if ([openDlg runModal] == NSModalResponseOK) {
             for (NSURL* url in [openDlg URLs]) {
-                result.emplace_back((char*) [[url path] UTF8String]);
+                paths.push_back(std::filesystem::path{(char*) [[url path] UTF8String]});
             }
         }
+
+        file_dialog_result result;
+        result.m_result = paths;
+        return result;
     }
 }
